0d7ab9647ea2a0e9685ba39ba803f275be017232
lnyaumwe23 <lnyaumwe23@amherst.edu>
2021-05-09 21:49:29 -0400

    wordcloud wrangled table data.

---
+++ wordcloudTables.R
@@ -0,0 +1,22 @@
+library(tidyverse) 
+library(janitor)
+
+library(tidytext)
+library(wordcloud)
+library(textdata)
+
+# Put in your actual path where the text files are saved
+mypath = "~/git/Stat231/Joe Biden speeches"
+setwd(mypath)
+
+# Create list of text files
+txt_files_ls = list.files(path=mypath, pattern="*.txt") 
+# Read the files in, assuming comma separator
+txt_files_df <- lapply(txt_files_ls, 
+                       function(x) {data.frame(title=substr(x,1,nchar(x)-4), 
+                                               text=read_file(x))})
+# Combine them
+speeches_df <- do.call("rbind", lapply(txt_files_df, as.data.frame))
+speeches_df
+
+
