6644d2d5f736afecdb08ae95f9ed607d55ca1d38
lnyaumwe23 <lnyaumwe23@amherst.edu>
2021-05-12 12:59:47 -0400

    finished wordcloud.

--- wordcloud.R
+++ wordcloud.R
@@ -8,13 +8,9 @@ library(textdata)
 
 # even using the URLs embedded in the HTML code, some did not work
 # remove the poems for which we don't have the text
-#path_in <- "/Users/lovemorenyaumwe/git/Stat231"
-trump_speeches <- read_csv("trump_speeches/trump_speeches_wordcloud.csv") %>%
-  filter(text != "Missing")
-trump_speeches
-# biden_speeches <- read_csv(paste0(path_in,"/biden_speeches_wordcloud.csv")) %>%
-# filter(text != "Missing")
-# biden_speeches
+
+trump_data <- "trump_speeches/trump_speeches_wordcloud.csv"
+biden_data <- "biden_speeches/biden_speeches_wordcloud.csv"
 
 
 # Create a list of custom stopwords that should be added
@@ -30,14 +26,19 @@ names(mystopwords) <- c("word", "lexicon")
 stop_words2 <-  dplyr::bind_rows(stop_words, mystopwords) %>%
   mutate(word = str_replace_all(word, "'","’"))
 
-word_frequencies <- trump_speeches %>%
-  unnest_tokens(output = word, input = text) %>%
-  anti_join(stop_words2, by="word") %>%
-  count(word, sort = TRUE) #%>%
-  #filter(word != "it's")%>%
-  #filter(word != "it’s")
+# function to draw word cloud
+dscore<-function(data){
+  speeches <- read_csv(data) %>%
+    filter(text != "Missing")
+  
+  word_frequencies <- speeches %>%
+    unnest_tokens(output = word, input = text) %>%
+    anti_join(stop_words2, by="word") %>%
+    count(word, sort = TRUE)
+  
+  wordcloud2(word_frequencies, shape = "star")
 
-wordcloud2(word_frequencies, shape = "star")
+}
 
-#word_frequencies %>%
- # filter(word == "it’s")
+dscore(trump_data)
+dscore(biden_data)
